f816245b1a06c9a0b0cfc5830ee7de3971f0c0b7
Peter Suhm <peter@suhm.dk>
2015-06-12 07:21:21 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -39,6 +39,20 @@ $(function() {
     ga('send', 'pageview');
 
 </script>
+<!-- Drip -->
+<script type="text/javascript">
+    var _dcq = _dcq || [];
+    var _dcs = _dcs || {};
+    _dcs.account = '9615953';
+
+    (function() {
+        var dc = document.createElement('script');
+        dc.type = 'text/javascript'; dc.async = true;
+        dc.src = '//tag.getdrip.com/9615953.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(dc, s);
+    })();
+</script>
 
 
 </body>
