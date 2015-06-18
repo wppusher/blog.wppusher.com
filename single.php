202407a50147c40db931de17f0a94dcb73165761
Peter Suhm <peter@suhm.dk>
2015-06-18 11:41:32 +0200

    Update single.php

--- single.php
+++ single.php
@@ -26,6 +26,20 @@
         <?php get_template_part('content', 'single'); ?>
 
     <?php endwhile; ?>
+    
+    <div id="disqus_thread"></div>
+<script type="text/javascript">
+    /* * * CONFIGURATION VARIABLES * * */
+    var disqus_shortname = 'wppusherblog';
+    
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function() {
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
 
 <?php endif; ?>
 
